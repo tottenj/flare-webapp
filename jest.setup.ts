e2404a03f4be0c1453a903d29f34caa1f15cf900
topsiteJosh <josh.totten8@gmail.com>
2025-05-05 23:14:13 -0400

    fucked up

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,17 +1,3 @@
 import '@testing-library/jest-dom';
 require('dotenv').config({path: '.env.test'})
 
-
-jest.mock('firebase/auth', () => ({
-  getAuth: () => ({
-    currentUser: null,
-    signInWithEmailAndPassword: jest.fn(),
-    createUserWithEmailAndPassword: jest.fn(),
-    signOut: jest.fn(),
-  }),
-}));
-
-jest.mock('firebase/app', () => ({
-  initializeApp: jest.fn(),
-  getApps: () => [],
-}));
