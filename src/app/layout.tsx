f2f66c3eeeb24ae6052fc57d2569e90a520d6801
topsiteJosh <josh.totten8@gmail.com>
2025-05-08 23:09:53 -0400

    added gitlab file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,8 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const { currentUser } = await getAuthenticatedAppForUser();
 
+  
+
   const userData = currentUser
     ? {
         uid: currentUser.uid,
