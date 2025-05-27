1b6d8417b634cb1d23471876a10449bd0a96a9f9
topsiteJosh <josh.totten8@gmail.com>
2025-05-27 19:55:57 -0400

    added files

---
+++ src/app/events/@modal/(.)[slug]/page.tsx
@@ -0,0 +1,9 @@
+import Modal from '@/components/modals/mainModal/MainModal';
+
+export default async function page({ params }: { params: { slug: string } }) {
+  return (
+    <Modal isOpen={true} route={true}>
+      <div>slkfdjslkj</div>
+    </Modal>
+  );
+}
