69ae8d50c5b701ad84997869473d2d9dae768afd
topsiteJosh <josh.totten8@gmail.com>
2025-05-28 20:31:06 -0400

    done

--- src/app/events/@modal/(.)[slug]/page.tsx
+++ src/app/events/@modal/(.)[slug]/page.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/lib/firebase/auth/configs/getFirestoreFromServer';
 import Image from 'next/image';
 import Link from 'next/link';
+import EventInfo from '../../../../components/events/EventInfo';
 
 export default async function page({ params }: { params: Promise<{ slug: string }> }) {
   const { firestore, storage } = await getServicesFromServer();
@@ -19,42 +20,7 @@ export default async function page({ params }: { params: Promise<{ slug: string 
 
   return (
     <Modal isOpen={true} route={true}>
-      <div className="flex w-full flex-col items-center h-auto">
-        <h1>{event.title}</h1>
-        <p>{org?.name ? 'Hosted By ' + org.name : ''}</p>
-        <div className="mt-4 flex w-full gap-8">
-          {img && (
-            <div className="relative h-auto min-h-[400px] w-full overflow-hidden rounded-xl shadow-md md:w-1/2">
-              <Image
-                src={img}
-                alt={event.title}
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, 50vw"
-                priority
-              />
-            </div>
-          )}
-          <div className="flex flex-col justify-between">
-            <div className="flex flex-col gap-2 text-lg">
-              <p>
-                <b>Date:</b> {event.startdate.toDateString()}
-              </p>
-              <p>
-                <b>Time:</b> {event.startdate.toLocaleTimeString()}
-              </p>
-              <p>
-                <b>Location:</b> {event.location.name}
-              </p>
-              <p>
-                <b>Cost:</b> {event.price == 0 ? 'Free' : event.price}
-              </p>
-              <p className="mt-4">{event.description}</p>
-            </div>
-            {event.ticketLink && <Link href={event.ticketLink}>Purchase Tickets</Link>}
-          </div>
-        </div>
-      </div>
+      <EventInfo />
     </Modal>
   );
 }
