d1d42c42d59920e1440778a7d866f66a5adf67eb
topsiteJosh <josh.totten8@gmail.com>
2025-07-13 23:41:14 -0400

    added files

---
+++ src/components/events/eventInfo/EventInfo.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from '@storybook/react';
+
+import EventInfo from './EventInfo';
+import Event from '@/lib/classes/event/Event';
+import FlareOrg from '@/lib/classes/flareOrg/FlareOrg';
+
+export default {
+  component: EventInfo,
+} satisfies Meta<typeof EventInfo>;
+
+type Story = StoryObj<typeof EventInfo>;
+
+export const Default: Story = {
+  args: {
+    img: '/prideHeart.png',
+    event: Event.sampleEvents[0],
+    org: FlareOrg.sampleOrg,
+  },
+};
+
+export const noImage: Story = {
+    args:{
+        img: null,
+        event: Event.sampleEvents[0],
+        org: FlareOrg.sampleOrg
+    }
+}
+
---
+++ src/components/events/eventInfo/EventInfo.tsx
@@ -0,0 +1,65 @@
+'use server';
+import Event from '@/lib/classes/event/Event';
+import Image from 'next/image';
+import FlareOrg from '@/lib/classes/flareOrg/FlareOrg';
+import { getServicesFromServer } from '@/lib/firebase/auth/configs/getFirestoreFromServer';
+import FlareUser from '@/lib/classes/flareUser/FlareUser';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faClock } from '@fortawesome/free-regular-svg-icons';
+import { faLocationDot, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import SecondaryHeader from '../../info/toolTip/secondaryHeader/SecondaryHeader';
+
+interface EventInfoProps {
+  img?: string | null;
+  event: Event;
+  org?: FlareOrg | null;
+}
+export default async function EventInfo({ img, event, org }: EventInfoProps) {
+  const formattedDate = event.startdate.toLocaleDateString('en-US', {
+    weekday: 'short', // Tue
+    month: 'long', // June
+    day: 'numeric', // 19
+    year: 'numeric', // 2025
+  });
+
+  return (
+    <div className="mx-auto flex h-full max-w-5xl flex-col p-4">
+      <div className="grid items-start gap-6 rounded-2xl bg-white p-6 shadow-lg transition-all duration-300 hover:shadow-xl md:grid-cols-[1fr_1.2fr]">
+        <div className="relative aspect-[4/3] max-h-[500px] w-full overflow-hidden rounded-xl border border-gray-200 shadow-sm md:aspect-[3/4]">
+          {img && <Image src={img} alt="Event Image" fill className="object-cover object-top" />}
+        </div>
+        <div className="flex flex-col">
+          <h2 className="mb-4 text-center text-3xl font-bold md:text-4xl">{event.title}</h2>
+          <div className="flex flex-col gap-2 text-sm text-gray-700"></div>
+          <SecondaryHeader header="Hosted By:" text={org?.name} />
+          <SecondaryHeader header={<FontAwesomeIcon icon={faCalendar} />} text={formattedDate} />
+          <SecondaryHeader
+            header={<FontAwesomeIcon icon={faClock} />}
+            text={event.startdate.toLocaleTimeString()}
+          />
+          <SecondaryHeader
+            header={<FontAwesomeIcon icon={faLocationDot} />}
+            text={event.location.name}
+          />
+          <SecondaryHeader
+            header={<FontAwesomeIcon icon={faDollarSign} />}
+            text={event.price.toString()}
+          />
+          <SecondaryHeader header={<FontAwesomeIcon icon={faUsers} />} text={event.ageGroup} />
+          <div className="mt-4 text-sm leading-relaxed text-gray-800">{event.description}</div>
+        </div>
+      </div>
+
+      {event.ticketLink && (
+        <Link
+          className="bg-primary hover:border-primary hover:text-primary mt-8 w-full self-center rounded-xl px-5 py-3 text-center text-lg font-semibold text-white transition hover:border-4 hover:bg-white"
+          href={event.ticketLink}
+        >
+          🎟 Purchase Ticket
+        </Link>
+      )}
+    </div>
+  );
+}
