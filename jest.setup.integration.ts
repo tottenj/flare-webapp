c17da1916b6cd0caa8674a22df5b4596af8c9f40
topsiteJosh <josh.totten8@gmail.com>
2025-11-03 22:50:31 -0500

    changes

--- jest.setup.integration.ts
+++ jest.setup.integration.ts
@@ -5,17 +5,17 @@ import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import prisma from '@/lib/prisma/prisma';
 import { resetTestDb } from './__tests__/utils/restTestDb';
 
-const app = initializeApp({
-  projectId: process.env.FIREBASE_PROJECT_ID,
-});
+//const app = initializeApp({
+  //projectId: process.env.FIREBASE_PROJECT_ID,
+//});
 
-const auth = getAuth(app);
-const firestore = getFirestore(app);
+//const auth = getAuth(app);
+//const firestore = getFirestore(app);
 
 // Connect to Firebase emulators once before all tests
 beforeAll(async () => {
-  connectAuthEmulator(auth, 'http://127.0.0.1:9099');
-  connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+ // connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+  //connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
 
   await prisma.$connect();
   // const res = await fetch('http://127.0.0.1:5001/flare-7091a/us-central1/seedAuthEmulator', {
