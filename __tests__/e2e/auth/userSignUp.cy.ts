867d5665c35530192c8cc76898194a63993192d1
topsiteJosh <josh.totten8@gmail.com>
2025-12-23 20:41:48 -0500

    first e2e test done

--- __tests__/e2e/auth/userSignUp.cy.ts
+++ __tests__/e2e/auth/userSignUp.cy.ts
@@ -4,8 +4,8 @@ describe('Successful Flow', () => {
   });
 
   it('Tests success flow', () => {
-    cy.get('[data-cy="email-input"]').type('test@gmail.com');
-    cy.get('[data-cy="password-input"]').type('password123');
+    cy.get('[data-cy="email-input"]').type('test@gmail.com', {force: true});
+    cy.get('[data-cy="password-input"]').type('password123', {force: true});
     cy.get('[data-cy="submit-button"]').click();
     cy.url().should('include', '/confirmation');
   });
