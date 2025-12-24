475e95130f0ba20d029069b3ace18f486ef7d8e8
topsiteJosh <josh.totten8@gmail.com>
2025-12-23 21:36:35 -0500

    added not disabled

--- __tests__/e2e/auth/userSignUp.cy.ts
+++ __tests__/e2e/auth/userSignUp.cy.ts
@@ -7,11 +7,11 @@ describe('Successful Flow', () => {
     const email = 'test@gmail.com';
     const password = 'password123';
 
-    cy.get('[data-cy="email-input"]').should('be.visible').type(email);
+    cy.get('[data-cy="email-input"]').should('be.visible').type(email, {force: true});
 
-    cy.get('[data-cy="password-input"]').should('be.visible').type(password);
+    cy.get('[data-cy="password-input"]').should('be.visible').type(password, {force: true});
 
-    cy.get('[data-cy="submit-button"]').should('not.be.disabled').click();
+    cy.get('[data-cy="submit-button"]').should('not.be.disabled').click({force: true});
 
     cy.url().should('include', '/confirmation');
     cy.contains('Thank You For Signing Up!').should('exist');
