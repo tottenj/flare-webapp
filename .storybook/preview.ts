083796e3bcedfa89808034d9e2218244bac8e4e2
topsiteJosh <josh.totten8@gmail.com>
2025-12-24 10:17:42 -0500

    began storybook configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,9 @@
 import { definePreview } from '@storybook/nextjs-vite';
 import '../src/app/globals.css';
 import './fonts.css'
-import { initialize } from 'msw-storybook-addon';
 import addonA11y from '@storybook/addon-a11y';
 
-initialize();
+
 
 export default definePreview({
   addons: [addonA11y()],
