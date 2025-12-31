98761e72a29fded39c9277f8efe445437445655c
topsiteJosh <josh.totten8@gmail.com>
2025-12-30 23:52:49 -0500

    changes

--- __tests__/e2e/auth/userSignUp.cy.ts
+++ __tests__/e2e/auth/userSignUp.cy.ts
@@ -11,7 +11,7 @@ describe('User Sign Up = Successful Flow', () => {
     cy.get('[data-cy="password-input"]').type(password, { force: true });
     cy.get('[data-cy="submit-button"]').click({ force: true });
     cy.recivedOobCode(email);
-    cy.location('pathname', { timeout: 10000 }).should('eq', '/confirmation');
+    cy.contains('Thank You For Signing Up!', { timeout: 20000 }).should('be.visible');
   });
 });
 
