4430e9719b1d67e6039f2273821ae964ff05c977
topsiteJosh <josh.totten8@gmail.com>
2026-02-18 19:44:39 -0500

    create event test flow complete

--- src/components/inputs/hero/number/HeroNumberInput.tsx
+++ src/components/inputs/hero/number/HeroNumberInput.tsx
@@ -2,5 +2,5 @@ import { NumberInput, NumberInputProps } from '@heroui/react';
 
 interface heroNumberInputProps extends NumberInputProps {}
 export default function HeroNumberInput({ ...props }: heroNumberInputProps) {
-  return <NumberInput className='rounded-none' classNames={{ inputWrapper: "rounded-2xl"}} {...props} />;
+  return <NumberInput data-cy={`${props.name ?? "price"}-input`} className='rounded-none' classNames={{ inputWrapper: "rounded-2xl"}} {...props} />;
 }
