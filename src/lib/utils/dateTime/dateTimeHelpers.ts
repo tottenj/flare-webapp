53185bc8415efa9f62fa48055f01b344f1565e57
topsiteJosh <josh.totten8@gmail.com>
2026-03-02 22:22:47 -0500

    updated dependencies

--- src/lib/utils/dateTime/dateTimeHelpers.ts
+++ src/lib/utils/dateTime/dateTimeHelpers.ts
@@ -1,11 +1,11 @@
-import { parseZonedDateTime } from "@internationalized/date";
+import { parseZonedDateTime } from '@internationalized/date';
 
-export function parseZonedToUTC(input: string):{utcDate:Date, timeZone:string}{
-    const zdt = parseZonedDateTime(input)
-    return{
-        utcDate: zdt.toDate(),
-        timeZone: zdt.timeZone
-    }
+export function parseZonedToUTC(input: string): { utcDate: Date; timeZone: string } {
+  const zdt = parseZonedDateTime(input);
+  return {
+    utcDate: zdt.toDate(),
+    timeZone: zdt.timeZone,
+  };
 }
 
 export function parseZonedToISO(
@@ -13,12 +13,16 @@ export function parseZonedToISO(
   locale: Intl.LocalesArgument = 'en-CA'
 ): { isoDate: string; timeString: string; timeZone: string } {
   const dt = parseZonedDateTime(input);
+  const jsDate = dt.toDate();
+
   return {
-    isoDate: dt.toDate().toISOString(),
-    timeString: dt.toDate().toLocaleTimeString(locale, {
+    isoDate: jsDate.toISOString(),
+    timeZone: dt.timeZone,
+    timeString: jsDate.toLocaleTimeString(locale, {
       hour: '2-digit',
       minute: '2-digit',
+      hour12: true,
+      timeZone: dt.timeZone,
     }),
-    timeZone: dt.timeZone,
   };
 }
