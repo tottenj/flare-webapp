1aa7e70264a2c999f435d54c13a5c4ec7014fba7
topsiteJosh <josh.totten8@gmail.com>
2026-03-22 23:41:10 -0400

    added fi file

--- scripts/fix-prisma-imports.js
+++ scripts/fix-prisma-imports.js
@@ -85,9 +85,7 @@ function main() {
     fixImportsInFile(filePath);
   });
 
-  import fs from 'fs';
-  import path from 'path';
-  import { fileURLToPath } from 'url';
+
 
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
