335154c5429f0a0cc71af5146312dd06dc726078
topsiteJosh <josh.totten8@gmail.com>
2026-03-22 23:58:36 -0400

    added fi file

--- src/app/api/events/[eventId]/edit-data/route.ts
+++ src/app/api/events/[eventId]/edit-data/route.ts
@@ -1,21 +1,21 @@
-import { AppError } from '@/lib/errors/AppError';
-import { EditEventData } from '@/lib/schemas/event/editEventDataSchema';
 import { UserContextService } from '@/lib/services/userContextService/userContextService';
-import { ActionResult } from '@/lib/types/ActionResult';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ eventId: string }> }
-): Promise<ActionResult<EditEventData>> {
+): Promise<NextResponse> {
   const { eventId } = await params;
   const ctx = await UserContextService.requireOrg();
   const actor = UserContextService.getOrgActor(ctx);
 
-  return fail(
-    new AppError({
-      code: 'UNKNOWN',
-      clientMessage: 'Something went wrong. Please try again.',
-    })
+  return NextResponse.json(
+    {
+      error: {
+        code: 'DLSKjf',
+        message: 'Ldfksjf',
+      },
+    },
+    { status: 500 }
   );
 }
